5d9b673b5af371c56a13f17e0e685564e61e8e3c
GABA-MANO <ybgabamano@gmail.com>
2022-03-16 17:20:08 +0100

    Ajout entités country et state + repository

---
+++ src/main/java/fr/lu_div/ecom/entity/Country.java
@@ -0,0 +1,38 @@
+package fr.lu_div.ecom.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+   private  String code;
+
+    @Column(name = "name")
+   private String name;
+
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+
+
+}
