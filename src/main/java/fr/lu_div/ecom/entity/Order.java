d9d1dca36df2f7efb1d281cee02e90fc2b60d362
GABA-MANO <ybgabamano@gmail.com>
2022-03-31 10:47:30 +0200

    Mise en place des relations entre les entités

--- src/main/java/fr/lu_div/ecom/entity/Order.java
+++ src/main/java/fr/lu_div/ecom/entity/Order.java
@@ -7,7 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.util.Date;
+import java.util.*;
 
 @Getter
 @Setter
@@ -38,5 +38,32 @@ public class Order {
     @Column(name = "last_updated")
     @UpdateTimestamp
     private Date lastUpdated;
+
+    @OneToMany(mappedBy = "orderItems", cascade = CascadeType.ALL)
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private  Customer customer;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "shipping_address_id", referencedColumnName = "id")
+    private  Address shippingAddress;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "billing_address_id", referencedColumnName = "id")
+    private  Address billingAddress;
+
+
+    public void add(OrderItem item) {
+        if (item != null) {
+            if (orderItems == null) {
+                orderItems = new HashSet<>();
+            }
+
+            orderItems.add(item);
+            item.setOrder(this);
+        }
+    }
 
 }
